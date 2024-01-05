afc204e03e32aa250a1e6a36b6344cc2583ef567
wumingzhao1223 <wumingzhao1223@gmail.com>
2024-01-06 07:57:23 +0800

    update

--- printft/main.c
+++ printft/main.c
@@ -1,4 +1,27 @@
+#include <stdio.h>
+
+#define TYPENAME(T) _Generic((T),                               \
+    int:"int",                                                  \
+    char:"char",                                                \
+    char*:"char*",                                              \
+    const char*:"const char*",                                  \
+    default:"?"                                                 \
+)
+
 int main(void)
 {
+    int i = 1;
+    printf("%s\n", TYPENAME(i));
+    printf("%s\n", TYPENAME(1));
+    char c = 'a';
+    printf("%s\n", TYPENAME(c));
+    printf("%s\n", TYPENAME('a'));
+    printf("%s\n", TYPENAME((char)97));
+    char* s = "string";
+    printf("%s\n", TYPENAME(s));
+    printf("%s\n", TYPENAME("string"));
+    const char* cs = "cstring";
+    printf("%s\n", TYPENAME(cs));
+    printf("%s\n", TYPENAME((const char*)"cstring"));
     return 0;
 }
